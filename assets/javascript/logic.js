63aa7468473e5084ab9c7d5f9e4c9fdabd6a7e1f
trevino <rafael.trevino@me.com>
2018-12-18 13:38:19 -0500

    fixed audio divs

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -6,10 +6,6 @@ $(document).ready(function() {
 
 // ********** LOGIC FOR PRONUNCIATION **********
 var myArray = [];
-var uniqueArray = [];
-var flValue = "";
-var writtenPron = "";
-var audioPron = "";
 
 function displayPronunciation() {
     // Grab the term from the input form
@@ -21,7 +17,6 @@ function displayPronunciation() {
         url: queryUrl + word + key,
         method:"GET"
     }).then(function(response) {
-        console.log(response.length);
         // Populate the array
         // For each item in the response array
         for (var i=0; i<response.length; i++) {
@@ -31,62 +26,41 @@ function displayPronunciation() {
             } else {
                 var flValue = response[i].fl;
                 var prsArray = response[i].hwi.prs;
-                console.log(response.length); 
-                console.log(flValue);
                 // For each item in the prs array
                 for (var j=0; j<prsArray.length; j++) {
                     // Skip if the prs doesn't have a sound parameter with it
-                    console.log(prsArray.length);
                     var wordArray = [];
                     if (prsArray[j].sound == undefined) {
                         // Skip here if undefined
                     } else if ((myArray.toString()).includes(prsArray[j].sound.audio) == true) {
-                        console.log(prsArray[j].sound.audio)
-                        console.log(myArray.toString());
-                        console.log(prsArray[j].sound.audio.indexOf(myArray.toString()));
                     } else {
-                        console.log(prsArray[j].sound.audio)
-                        console.log(myArray.toString());
-                        console.log(prsArray[j].sound.audio.indexOf(myArray.toString()));
                         // If it has a sound parameter, grab the written pronunciation and audio file name, and along with flValue, send to myArray
                         var writtenPron = prsArray[j].mw;
                         var audioPron = prsArray[j].sound.audio;
-                        console.log(flValue);
-                        console.log(writtenPron);
-                        console.log(audioPron);
-                        console.log(j);
-                        console.log(myArray);
                         // Push the fl (pos), mw (written pronunciation) and audio (sound file name) into an array
                         wordArray.push(flValue, writtenPron, audioPron);
                         myArray.push(wordArray);
                     }; // End of nested if statement
                 }; // End of nested for statement
             }; // End of first if statement
-            console.log(myArray.length);
         }; // End of for response.length statement
 
-        // Remove duplicates from the array
-        // Need a function or something for this
-
-        // Create the Divs
-        // Source for play icon: https://iconmonstr.com/media-control-4-png/
+        // Create Divs for each item in myArray
         for (var i=0; i < myArray.length; i++) {
-            var d = $("<div>");
+            var dAudio = $("<div>").addClass("audioDiv");
+            var dPron = $("<div>").addClass("pronDiv");
+            var dPos = $("<div>").addClass("posDiv");
+            var dPronGroup = $("<div>").addClass("pronGroupDiv");
             var p = $("<p>");
             var a = $("<a>");
-            // var img = $("<img src=\"./assets/images/play.png\" alt=\"Play\">");
-            console.log(myArray[i]);
             var pos = myArray[i][0];
             var pron = myArray[i][1];
             var file = myArray[i][2];
-            console.log(pos, pron, file);
-            // a.append(img);
-            // Create audio file link
+            // Logic for subdirectory in order to create url
             var uniChar = file.slice(0,1);
             var biChar = file.slice(0,2);
             var triChar = file.slice(0,3);
             var subDir = "";
-            console.log(uniChar);
             if (triChar == "bix") {
                 subDir = "bix";
             } else if (biChar == "gg") {
@@ -106,15 +80,14 @@ function displayPronunciation() {
                 }
             }
             var url = "https://media.merriam-webster.com/soundc11/" + subDir + "/" + file + ".wav";
-            console.log(url);
-            a.attr("href", url);
-            p.html(pron);
-            p.append(a);
-            d.append(p);
-            $("#pronDiv").append(d);
-            $("#audioDiv").append("<audio id=\"embed_player\" src=" + url + " autostart=\"false\" controls=\"true\"></audio>");
-            console.log(d);
-        } // End of for myArray.length statement        
+            dAudio.append("<audio id=\"embed_player\" src=" + url + " autostart=\"false\" controls=\"true\"></audio>");
+            dPron.append(pron);
+            dPos.append(pos);
+            dPronGroup.prepend(dAudio);
+            dPronGroup.prepend(dPron);
+            dPronGroup.prepend(dPos);
+            $("#pronunciations").append(dPronGroup);
+        } // End of for myArray.length statement       
     }); // End of Ajax function
 } // End of displayPronunciation function
 
